ed1cb4a8e35a541bf9bfd8b0a1c3f9203ec6a345
Ian Lenehan <ianlenehan@gmail.com>
2022-02-02 20:34:12 +0000

    finish local storage

--- js/main.js
+++ js/main.js
@@ -1,9 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-  initHelpModal();
-  initStatsModal();
-  createSquares();
-  addKeyboardClicks();
-
   // also in local storage
   let currentWordIndex = 0;
   let guessedWordCount = 0;
@@ -12,6 +7,62 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const words = ["sweet", "onion", "shoes", "heavy", "couch"];
   let currentWord = words[currentWordIndex];
+
+  initLocalStorage();
+  initHelpModal();
+  initStatsModal();
+  createSquares();
+  addKeyboardClicks();
+  loadLocalStorage();
+
+  function initLocalStorage() {
+    const storedCurrentWordIndex =
+      window.localStorage.getItem("currentWordIndex");
+    if (!storedCurrentWordIndex) {
+      window.localStorage.setItem("currentWordIndex", currentWordIndex);
+    } else {
+      currentWordIndex = Number(storedCurrentWordIndex);
+      currentWord = words[currentWordIndex];
+    }
+  }
+
+  function loadLocalStorage() {
+    currentWordIndex =
+      Number(window.localStorage.getItem("currentWordIndex")) ||
+      currentWordIndex;
+    guessedWordCount =
+      Number(window.localStorage.getItem("guessedWordCount")) ||
+      guessedWordCount;
+    availableSpace =
+      Number(window.localStorage.getItem("availableSpace")) || availableSpace;
+    guessedWords =
+      JSON.parse(window.localStorage.getItem("guessedWords")) || guessedWords;
+
+    currentWord = words[currentWordIndex];
+
+    const storedBoardContainer = window.localStorage.getItem("boardContainer");
+    if (storedBoardContainer) {
+      document.getElementById("board-container").innerHTML =
+        storedBoardContainer;
+    }
+
+    const storedKeyboardContainer =
+      window.localStorage.getItem("keyboardContainer");
+    if (storedKeyboardContainer) {
+      document.getElementById("keyboard-container").innerHTML =
+        storedKeyboardContainer;
+
+      addKeyboardClicks();
+    }
+  }
+
+  function resetGameState() {
+    window.localStorage.removeItem("guessedWordCount");
+    window.localStorage.removeItem("guessedWords");
+    window.localStorage.removeItem("keyboardContainer");
+    window.localStorage.removeItem("boardContainer");
+    window.localStorage.removeItem("availableSpace");
+  }
 
   function createSquares() {
     const gameBoard = document.getElementById("board");
@@ -23,6 +74,19 @@ document.addEventListener("DOMContentLoaded", () => {
       square.setAttribute("id", i + 1);
       gameBoard.appendChild(square);
     }
+  }
+
+  function preserveGameState() {
+    window.localStorage.setItem("guessedWords", JSON.stringify(guessedWords));
+
+    const keyboardContainer = document.getElementById("keyboard-container");
+    window.localStorage.setItem(
+      "keyboardContainer",
+      keyboardContainer.innerHTML
+    );
+
+    const boardContainer = document.getElementById("board-container");
+    window.localStorage.setItem("boardContainer", boardContainer.innerHTML);
   }
 
   function getCurrentWordArr() {
@@ -43,14 +107,27 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
+  function updateTotalGames() {
+    const totalGames = window.localStorage.getItem("totalGames") || 0;
+    window.localStorage.setItem("totalGames", Number(totalGames) + 1);
+  }
+
   function showResult() {
     const finalResultEl = document.getElementById("final-score");
     finalResultEl.textContent = "Wordle 1 - You win!";
+
+    const totalWins = window.localStorage.getItem("totalWins") || 0;
+    window.localStorage.setItem("totalWins", Number(totalWins) + 1);
+
+    const currentStreak = window.localStorage.getItem("currentStreak") || 0;
+    window.localStorage.setItem("currentStreak", Number(currentStreak) + 1);
   }
 
   function showLosingResult() {
     const finalResultEl = document.getElementById("final-score");
     finalResultEl.textContent = `Wordle 1 - Unsuccessful Today!`;
+
+    window.localStorage.setItem("currentStreak", 0);
   }
 
   function clearBoard() {
@@ -123,6 +200,11 @@ document.addEventListener("DOMContentLoaded", () => {
     return "incorrect-letter";
   }
 
+  function updateWordIndex() {
+    console.log({ currentWordIndex });
+    window.localStorage.setItem("currentWordIndex", currentWordIndex + 1);
+  }
+
   async function handleSubmitWord() {
     const currentWordArr = getCurrentWordArr();
     const guessedWord = currentWordArr.join("");
@@ -148,6 +230,8 @@ document.addEventListener("DOMContentLoaded", () => {
       // }
       const firstLetterId = guessedWordCount * 5 + 1;
 
+      localStorage.setItem("availableSpace", availableSpace);
+
       const interval = 200;
       currentWordArr.forEach((letter, index) => {
         setTimeout(() => {
@@ -161,10 +245,15 @@ document.addEventListener("DOMContentLoaded", () => {
             const keyboardEl = document.querySelector(`[data-key=${letter}]`);
             keyboardEl.classList.add(tileClass);
           }
+
+          if (index === 4) {
+            preserveGameState();
+          }
         }, index * interval);
       });
 
       guessedWordCount += 1;
+      window.localStorage.setItem("guessedWordCount", guessedWordCount);
 
       if (guessedWord === currentWord) {
         setTimeout(() => {
@@ -172,6 +261,9 @@ document.addEventListener("DOMContentLoaded", () => {
           if (okSelected) {
             clearBoard();
             showResult();
+            updateWordIndex();
+            updateTotalGames();
+            resetGameState();
           }
           return;
         }, 1200);
@@ -185,6 +277,9 @@ document.addEventListener("DOMContentLoaded", () => {
           if (okSelected) {
             clearBoard();
             showLosingResult();
+            updateWordIndex();
+            updateTotalGames();
+            resetGameState();
           }
           return;
         }, 1200);
@@ -261,6 +356,19 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
+  function updateStatsModal() {
+    const currentStreak = window.localStorage.getItem("currentStreak");
+    const totalWins = window.localStorage.getItem("totalWins");
+    const totalGames = window.localStorage.getItem("totalGames");
+
+    document.getElementById("total-played").textContent = totalGames;
+    document.getElementById("total-wins").textContent = totalWins;
+    document.getElementById("current-streak").textContent = currentStreak;
+
+    const winPct = Math.round((totalWins / totalGames) * 100) || 0;
+    document.getElementById("win-pct").textContent = winPct;
+  }
+
   function initStatsModal() {
     const modal = document.getElementById("stats-modal");
 
@@ -272,7 +380,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // When the user clicks on the button, open the modal
     btn.addEventListener("click", function () {
-      // update stats here
+      updateStatsModal();
       modal.style.display = "block";
     });
 
